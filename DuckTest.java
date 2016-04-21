92a4857b7070079262460af8036bda42636bfb1f
shenlijun1992 <352704969@qq.com>
2016-04-21 13:25:18 +0800

    Update DuckTest.java

--- DuckTest.java
+++ DuckTest.java
@@ -5,14 +5,14 @@ import com.strategy.cn.spaceDuck;
 
 public class DuckTest {
 	public static void main(String[] args) {
-		System.out.println("测试鸭子程序开始...");
+		System.out.println("test is stating...");
 		System.out.println("*********");
 		Duck duck = new LvtouDuck();
 		duck.quack();
 		duck.display();
 		duck.fly();
 		System.out.println("*********");
-		System.out.println("测试鸭子程序结束...");
+		System.out.println("test is ending...");
 	}
 
 }
